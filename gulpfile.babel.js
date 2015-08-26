57e1ff918917d05a25529840befd6b88f192b227
Casey White <casey.r.white@gmail.com>
2015-08-26 14:22:03 -0500

    clean dist before build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,16 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import sequence from 'gulp-sequence';
+import rimraf from 'rimraf';
 
-gulp.task('build', () => {
+gulp.task('dist:clean', (cb) => {
+  return rimraf('./dist', cb);
+});
+
+gulp.task('dist:build', () => {
   return gulp.src('src/**/*.js')
-    .pipe(babel())
+    .pipe(babel({ babelrc: './.babelrc' }))
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('default', sequence('dist:clean', 'dist:build'));
