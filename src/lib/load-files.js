0fe44998db352ea80f7d0bc977bdb51c43618055
Casey White <casey.r.white@gmail.com>
2016-01-08 11:40:27 -0600

    remove clearhead module dependency

--- src/lib/load-files.js
+++ src/lib/load-files.js
@@ -1,6 +1,30 @@
 import fs from 'fs'
 import path from 'path'
-import { series } from 'clearhead/async'
+
+function noop() {}
+
+function dequeue(queue) {
+  return { current: queue[0], remaining: queue.slice(1) };
+}
+
+/**
+ * @desc series() applies an iterator to a series of items in an array.
+ *
+ * @param {Array} [queue=[]] - the array items to process
+ * @param {Function} iterator - the iterator to apply to each item
+ * @param {Function} done - callback to execute when series complete
+ */
+export function series(queue = [], iterator = noop, done = noop) {
+
+  function iterate(queue, iterator, done, ...args) {
+    const { current, remaining } = dequeue(queue);
+    const next = iterate.bind(null, remaining, iterator, done);
+
+    return current ? iterator(current, next, ...args) : done(...args);
+  }
+
+  return iterate(queue, iterator, done);
+}
 
 function loadFiles(paths = [], callback) {
   const filepaths = typeof paths === 'string' ? [paths] : paths
